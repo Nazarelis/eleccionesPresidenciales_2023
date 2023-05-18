647e8d347d6fcb51ee146a33fb5ea919f1a91233
nazarelis <anabelojeda2001@gmail.com>
2023-05-18 19:58:43 -0300

    Co-authored-by: Marilyn Vino <vinomarilyn@users.noreply.github.com> Co-authored-by: gpedrosad <gpedrosad@users.noreply.github.com>

--- ELECCIONES.py
+++ ELECCIONES.py
@@ -1,3 +1,4 @@
+import re # EXPRESIONES REGULARES
 def impresion(nombres,codigos):
     print("________________________________________________________________________")
     print("                          \033[1mREGIONES GEOGRAFICAS\033[0m        ")
@@ -13,6 +14,9 @@ def impresionDos(partidos,nombres,abv,lista):
     print("       \033[1mNOMBRE\033[0m                 \033[1mABREVIATURA\033[0m                  \033[1mLISTA\033[0m  ")
     print("________________________________________________________________________")
    
+# def busqueda(archivo,posDato,valor):
+#     for ():
+    
 
 def validarPartido(partido): ####  ✔✔✔ VALIDAMOS QUE EL PARTIDO NO SEA UN CARACTER VACÍO, FUNCIONA ✔ 
     while partido == "":
@@ -40,9 +44,10 @@ def validarNumeroLista(numero): ###✔✔✔ VALIDAMOS NUMEROlista ENTERO DISTIN
 
 
 try:
-    archPartido = open("C:\partidosPoliticos.csv","wt")        								###CREAMOS EL ARCHIVO
+    archPartido = open(r"C:\Users\ThinkPad\Desktop\partidosPoliticos.txt","wt")        			###CREAMOS EL ARCHIVO					
+    archPartidoLectura=open(r"C:\Users\ThinkPad\Desktop\partidosPoliticos.txt","rt")
 except IOError:
-    print("No se pudo crear el archivo")                   							###EXCEPCIÓN SI NO PODEMOS CREARLO (REVISAR UBICACION)
+    print("No se pudo encontrar el archivo")                   							###EXCEPCIÓN SI NO PODEMOS CREARLO (REVISAR UBICACION)
 else:
     partido = input("Ingrese nombre del partido, FIN para finalizar: ") 			###INGRESAMOS PARTIDO FUERA DE WHILE (CONDICIÓN DE FIN ACÁ)
     validarPartido(partido)															###VALIDAMOS PARTIDO
@@ -56,6 +61,15 @@ else:
         
         partido = input("Ingrese nombre del partido, FIN para finalizar: ") 		###VOLVEMOS A PEDIR PARTIDO, PARA REEMPEZAR O TERMINAR WHILE (CONDICIÓN DE FIN ACÁ)
         validarPartido(partido)														###VALIDAMOS PARTIDO
+    lines = archPartidoLectura.readlines()#PRUEBAS DE CONSULTA EN EL ARCHIVO
+    buscarPalabra = "palabra"
+    for x in lines:
+        buscar =re.search(buscarPalabra,x)
+        print(buscar)
+    print(lines)
+
+    archPartido.close()	
+    archPartidoLectura.close()
     
-    archPartido.close()															### TERMINADO EL WHILE, CERRAMOS EL ARCHIVO
+    														### TERMINADO EL WHILE, CERRAMOS EL ARCHIVO
 
