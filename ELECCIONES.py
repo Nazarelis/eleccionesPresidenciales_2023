be27b6ba2fc245bdc4f671fa020040c26d47e3fb
Nazarelis Ojeda <106442997+Nazarelis@users.noreply.github.com>
2023-05-20 17:06:35 -0300

    Update ELECCIONES.py

--- ELECCIONES.py
+++ ELECCIONES.py
@@ -1,23 +1,3 @@
-import re # EXPRESIONES REGULARES
-def impresion(nombres,codigos):
-    print("________________________________________________________________________")
-    print("                          \033[1mREGIONES GEOGRAFICAS\033[0m        ")
-    print("________________________________________________________________________")
-    print("                \033[1mNOMBRE\033[0m                      \033[1mCODIGO O ABREVIATURA\033[0m       ")
-    print("________________________________________________________________________")
-
-   
-def impresionDos(partidos,nombres,abv,lista):
-    print("________________________________________________________________________")
-    print("                          \033[1mPARTIDOS POLITICOS\033[0m        ")
-    print("________________________________________________________________________")
-    print("       \033[1mNOMBRE\033[0m                 \033[1mABREVIATURA\033[0m                  \033[1mLISTA\033[0m  ")
-    print("________________________________________________________________________")
-   
-# def busqueda(archivo,posDato,valor):
-#     for ():
-    
-
 def validarPartido(partido): ####  ✔✔✔ VALIDAMOS QUE EL PARTIDO NO SEA UN CARACTER VACÍO, FUNCIONA ✔ 
     while partido == "":
         partido = input("Valor vacío, reingresar partido")
