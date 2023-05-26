0968cc960ffd6ed4e753c7295e99d35e371f170d
nazarelis <anabelojeda2001@gmail.com>
2023-05-26 11:58:16 -0300

    cambio nombre funcion dni

--- fase_2_copa.py
+++ fase_2_copa.py
@@ -1,6 +1,6 @@
 import random
 ### INICIO BLOQUE DE CODIGO DE FUNCIONES DE REGISTRO
-def nroDni(nro):
+def generarNroDniRandom():
     #GENERA UN NUMERO AL AZAR VALOR ENTERO POSITIVO MENOR A 100000000
     nro = random.randint(0,99999999)
     return nro
@@ -8,9 +8,16 @@ def nroDni(nro):
 ### FIN BLOQUE DE CODIGO DE FUNCIONES DE REGISTRO
 
 
-"""
-PRUEBA PARA GENERAR EL NRO DE DNI en proceso---
-numero=int(input("seleccione 1 para ingresar su DNI: "))
-nro= nroDni(numero)
-print(nro)
-"""
+
+
+
+
+### PROGRAMA PRINCIPAL ###
+
+
+dni = generarNroDniRandom()
+#print(dni)
+
+
+### FIN PROGRAMA PRINCIPAL ###
+
