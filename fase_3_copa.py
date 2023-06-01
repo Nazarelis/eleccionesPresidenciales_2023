4b5c8a4082f94d2637ca39486b1c46e47b6f2337
gpedrosad <111534380+gpedrosad@users.noreply.github.com>
2023-06-01 10:51:19 -0300

    Update fase_3_copa.py

--- fase_3_copa.py
+++ fase_3_copa.py
@@ -17,7 +17,32 @@ def obtenerListaDeRegiones():
     return lista
 
 
-lista = obtenerListaDeRegiones()
+def obtenerListaDePartidos():
+    lista = []
+    try:                                                                    
+        archivoParaLeer = open("PartidosPoliticos.csv", "rt")                  
+    except IOError:                                                         
+        print("No se pudo encontrar archivo")                               
+    else:
+        registro = archivoParaLeer.readline()
+        while registro:
+            nombre, abreviatura,codigo = registro.split(";")
+            lista.append(nombre)
+            registro = archivoParaLeer.readline()
+    finally:
+        archivoParaLeer.close()
+    return lista
 
-print(lista)
+
+listaRegiones = obtenerListaDeRegiones()
+listaPartidos = obtenerListaDePartidos()
+
+print(listaRegiones)
+print(listaPartidos)
+
+
+
+
+listaCargos = ["PRESIDENTE Y VICEPRESIDENTE", "DIPUTADO", "SENADOR", "GOBERNADOR Y VICEGOBERNADOR"]
+
 
